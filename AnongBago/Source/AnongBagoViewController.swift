4356ab3a9539c3575348e0d5047df6658fd47039
John Raymund Catahay <raymund.cat@gmail.com>
2017-12-07 02:19:36 +0800

    added initial tableview and model

--- AnongBago/Source/AnongBagoViewController.swift
+++ AnongBago/Source/AnongBagoViewController.swift
@@ -8,6 +8,45 @@
 
 import UIKit
 
+public struct Update {
+    let title: String
+    let description: String
+}
+
 open class AnongBagoViewController: UIViewController {
     
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+    
+    open var updates: [Update] = [] {
+        didSet{
+            
+        }
+    }
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
+}
+
+extension AnongBagoViewController: UITableViewDelegate, UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return updates.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+    }
 }
